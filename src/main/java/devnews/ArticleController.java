4bb5e7f4565c62b33c44aa27cd70ed32f1785447
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-04-09 11:05:38 +0200

    Refactores to use RequestBody in create method

--- src/main/java/devnews/ArticleController.java
+++ src/main/java/devnews/ArticleController.java
@@ -1,10 +1,7 @@
 package devnews;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,9 +20,8 @@ public class ArticleController {
         return articleRepository.findAll();
     }
 
-    @PostMapping("/create/{title}/{body}/{authorName}")
-    public Article create(@PathVariable("title") String title, @PathVariable("body") String body, @PathVariable("authorName") String authorName) {
-        Article article = new Article(title, body, authorName);
+    @PostMapping("/create/")
+    public Article create(@RequestBody Article article) {
         articleRepository.save(article);
         return article;
     }
