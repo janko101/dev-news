e897b07ca38c624efd8679808df59051b8ef19fc
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-04-12 09:41:17 +0200

    Adds Commet model with body and authorNamr, generates getters and setters

---
+++ src/main/java/devnews/Comment.java
@@ -0,0 +1,41 @@
+package devnews;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String body;
+    private String authorName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
