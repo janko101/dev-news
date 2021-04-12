c0fd2ef7558ca390b7b3a88372e2f5ac9f981177
Janko Radakovic <jankoradakovic@sda-lund-67-mbpro.lan>
2021-04-12 15:37:22 +0200

    Adds TopicRepository and TopicController with post req to create a topic

---
+++ src/main/java/devnews/TopicController.java
@@ -0,0 +1,25 @@
+package devnews;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+    TopicRepository topicRepository;
+    ArticleRepository articleRepository;
+
+    public TopicController(TopicRepository topicRepository, ArticleRepository articleRepository) {
+        this.topicRepository = topicRepository;
+        this.articleRepository = articleRepository;
+    }
+
+    @PostMapping("/topics")
+    public ResponseEntity<Topic> create(@RequestBody Topic topic) {
+        topicRepository.save(topic);
+        return ResponseEntity.status(HttpStatus.CREATED).body(topic);
+    }
+}
---
+++ src/main/java/devnews/TopicRepository.java
@@ -0,0 +1,6 @@
+package devnews;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+}
